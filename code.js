5404af9295ce92826920cc443c4198067dc90cf1
A-an <36506662+A-an@users.noreply.github.com>
2018-12-18 21:23:28 +0000

    Create code.js

---
+++ code.js
@@ -0,0 +1,11 @@
+<script>
+  (function() {
+    var cx = '013036898865932059220:nbek2k9xdiw';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
